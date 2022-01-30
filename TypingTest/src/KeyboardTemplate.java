c2bb074ae8038518abd2973b75929429b845aad6
Vivian <74420191+VivTian2@users.noreply.github.com>
2022-01-30 14:40:42 -0500

    Update KeyboardTemplate.java

--- TypingTest/src/KeyboardTemplate.java
+++ TypingTest/src/KeyboardTemplate.java
@@ -1,7 +1,9 @@
-
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
@@ -10,15 +12,22 @@ import javax.swing.*;
  * @author [ insert author here ]
  *
  */
-public class KeyboardTemplate extends Play implements KeyListener {
+public class KeyboardTemplate extends Play implements KeyListener, ActionListener{
 
 	static JButton buttons[] = new JButton[57];
 	static int codes[] = new int[57];
 	private int kcode;
-	public static JTextArea textbox = new JTextArea();
-	static JFrame f = new JFrame();
-	/*
-	//testing sentences
+	  JTextArea textbox = new JTextArea();	
+
+	//fonts
+	 Font instrucFont = new Font("Verdana", Font.BOLD+Font.ITALIC, 16);
+	 Font backFont = new Font("Verdana", Font.BOLD+Font.ITALIC, 16);
+	 Font labelFont = new Font("Verdana", Font.BOLD+Font.ITALIC, 23);
+
+	//check to make sure stopwatch only starts once
+	static boolean startWatch = true;
+
+	// testing sentences
 	static JLabel easyText = new JLabel("The big ugly tree destroys the beauty of the house.");
 	static JLabel mediumText = new JLabel("Once you join me, we can go visit the aquarium at the mall.");
 	static JLabel hardText = new JLabel("He thought the movie was great except for the scene with the cantaloupe.");
@@ -26,42 +35,80 @@ public class KeyboardTemplate extends Play implements KeyListener {
 	static String compareEasy = "The big ugly tree destroys the beauty of the house.";
 	static String compareMedium = "Once you join me, we can go visit the aquarium at the mall.";
 	static String compareHard = "He thought the movie was great except for the scene with the cantaloupe.";
-	 */
-	//timer
+
 	static long startTime;
+	Timer timer;
+
+	JLabel timerDisplay = new JLabel();
+
+	static int totalTime = 0;
+	static int seconds = 0;
+	static int minutes = 0;
+	static int hours= 0;
+
+	String secDisplay = String.format("%02d", seconds);
+	String minDisplay = String.format("%02d", minutes);
+	String hourDisplay = String.format("%02d", hours);
 
 	/**
 	 * Creates a full QWERTY keyboard in frame f, as well as starts the timer
 	 * @param f target frame where a keyboard is needed
 	 */
-	public void displayKeyboard(JFrame f) {	
+	KeyboardTemplate() {	
 
-		if (whichLevel == 1) {
+		JFrame f = new JFrame();
+		f.setSize(810,600);
+		f.getContentPane().setBackground(Color.decode("#b7b7a4"));
+
+
+		JButton backBut = new JButton("BACK");
+		if (Main.whichLevel == 1) {
 			f.setTitle("Easy test");
-		} else if (whichLevel == 2) {
+			easyText.setSize(500,200);
+			easyText.setLocation(10,10);
+			easyText.setFont(labelFont);
+			f.add(easyText);
+
+		} else if (Main.whichLevel == 2) {
 			f.setTitle("Medium test");
-		} else if (whichLevel == 3) {
+		} else if (Main.whichLevel == 3) {
 			f.setTitle("Hard test");
 		} else {
 			f.setTitle("Custom test");
 		}
 
+		backBut.setBounds(670,16,80,40);
+		backBut.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == backBut) {
+					f.dispose();
+				}
 
-		f.setSize(810,600);
+			}
+		});
+
+		f.add(backBut);
 
 		//display instructions
-		JLabel instructions = new JLabel("Press any key to start, and enter (on your keyboard) when you're finished!") ;
-		instructions.setBounds(0,0,500,20);
-		f.add(instructions);
+		JLabel instructions = new JLabel("Press any key to start, and enter (on your keyboard)");
+		JLabel instructions2 = new JLabel ("when you're finished!") ;
+		instructions.setBounds(10,10,700,20);
+		instructions.setFont(instrucFont);
+		instructions.setForeground(Color.white);
+		//f.add(instructions);
+
+		instructions2.setBounds(10,35,500,20);
+		instructions2.setFont(instrucFont);
+		instructions2.setForeground(Color.white);
+		//f.add(instructions2);
 
 		//create text area for user to type in
-		textbox.setBounds(2,140,780,110);
+		textbox.setBounds(8,140,780,110);
 		f.add(textbox);
-
 
 		//display keyboard
 		//first row
-		//keyboardTemplate b = new keyboardTemplate(f);
 		makeButton("~", 5, 260, 52, 52, f, 0,192);
 		makeButton("1", 57, 260, 52, 52, f,1,49);
 		makeButton("2", 109, 260, 52, 52, f,2,50);
@@ -120,16 +167,140 @@ public class KeyboardTemplate extends Play implements KeyListener {
 		makeButton(",", 473, 416, 52, 52, f,49,44);
 		makeButton(".", 525, 416, 52, 52, f,50,46);
 		makeButton("?", 577, 416, 52, 52, f,51,47);
-		makeButton("â†‘", 655, 416, 52, 52, f,52,38);
+		makeButton("↑", 655, 416, 52, 52, f,52,38);
 
 		//last row
 		makeButton("", 208, 468, 312, 52, f,53,32);
-		makeButton("â†�", 604, 468, 52, 52, f,54,37);
-		makeButton("â†“", 654, 468, 52, 52, f,55,40);
-		makeButton("â†’", 706, 468, 52, 52, f,56,39);
+		makeButton("←", 604, 468, 52, 52, f,54,37);
+		makeButton("↓", 654, 468, 52, 52, f,55,40);
+		makeButton("→", 706, 468, 52, 52, f,56,39);
+
+		timerDisplay.setText(hourDisplay + ":" + minDisplay + ":" + secDisplay);
+		timerDisplay.setSize(300,200);
+		timerDisplay.setLocation(0,0);
+		f.add(timerDisplay);
+
+		timer = new Timer(1000, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				totalTime = totalTime + 1000;
+				hours = (totalTime / 3600000);
+				minutes = (totalTime / 60000) % 60;
+				seconds = (totalTime / 1000) % 60;
+				
+				secDisplay = String.format("%02d", seconds);
+				minDisplay = String.format("%02d", minutes);
+				hourDisplay = String.format("%02d", hours);
+				timerDisplay.setText(hourDisplay + ":" + minDisplay + ":" + secDisplay);
+
+			}});
+
+		//f.add(timerDisplay);
+
 
 		//add key listener
-		textbox.addKeyListener(new KeyboardTemplate());
+		textbox.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {				
+
+				if(Main.whichLevel == 1) {
+					if (startWatch == true) {
+						
+						start();
+						startWatch = false;		
+						System.out.println(timer.isRunning());
+						
+
+					}
+
+					if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+						long endTime = System.nanoTime();
+						long duration = endTime - startTime;
+						long seconds = (long) (duration/ 1000000000.0);
+						if(textbox.getText().trim().equals(compareEasy)) {
+							JOptionPane.showMessageDialog( f, "Correct! That took you" + seconds + " seconds");
+						}
+						textbox.setText("");
+						startWatch = true;
+						stop();
+						reset();
+						f.dispose();
+					}
+				}
+
+				if(Main.whichLevel == 2) {
+					if (startWatch == true) {
+						reset();
+						start();
+						startWatch = false;
+					}
+
+					if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+						long endTime = System.nanoTime();
+						long duration = endTime - startTime;
+						long seconds = (long) (duration/ 1000000000.0);
+						if(textbox.getText().trim().equals(compareMedium)) {
+							JOptionPane.showMessageDialog( f, "Correct! That took you" + seconds + " seconds");
+						}
+						textbox.setText("");
+
+						startWatch = true;
+
+						f.dispose();
+
+					}
+				}
+
+				if(Main.whichLevel == 3) {
+
+					if (startWatch == true) {
+						reset();
+						start();
+						startWatch = false;
+					}
+
+					if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+						long endTime = System.nanoTime();
+						long duration = endTime - startTime;
+						long seconds = (long) (duration/ 1000000000.0);
+						if(textbox.getText().trim().equals(compareHard)) {
+							JOptionPane.showMessageDialog( f, "Correct! That took you" + seconds + " seconds");
+						}
+						textbox.setText("");
+						startWatch = true;
+						f.dispose();
+					}
+				}
+
+
+				//find which key is being pressed and change the colour
+				for (int i = 0; i < 57; i++) {
+					if (codes[i] == e.getKeyCode()) {
+						buttons[i].setBackground(Color.decode("#ccd5ae"));
+					}
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				for (int i = 0; i < 57; i++) {
+					if (codes[i] == e.getKeyCode()) {
+						buttons[i].setBackground(Color.decode("#a6a397"));
+
+					}
+				}
+
+
+			}
+
+
+		});
 
 		f.setLayout(null);
 		f.setVisible(true);
@@ -150,8 +321,12 @@ public class KeyboardTemplate extends Play implements KeyListener {
 	 * @param buttonNum button ID number
 	 * @param kcode button keyboard code
 	 */
+
 	public void makeButton(String n, int x, int y, int w, int h, JFrame f, int buttonNum, int kcode) {
 		JButton buttonName = new JButton(n);
+		buttonName.setBackground(Color.decode("#a6a397"));
+		buttonName.setBorder(BorderFactory.createLineBorder(Color.white, 5));
+		buttonName.setForeground(Color.white);
 		buttonName.setBounds(x,y,w,h);
 		buttons[buttonNum] = buttonName;
 		codes[buttonNum] = kcode;
@@ -159,68 +334,46 @@ public class KeyboardTemplate extends Play implements KeyListener {
 
 	}
 
-	@Override
-	public void keyTyped(KeyEvent e) {
+	void start() {
+		timer.start();
+	}
+
+	void stop() {
+		timer.stop();
+	}
+
+
+	void reset() {
+		System.out.println("Reset");
+		totalTime = 0;
+		seconds = 0;
+		minutes = 0;
+		hours= 0;
 
 	}
 
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 
-		if(Main.whichLevel == 1) {
-			if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-				long endTime = System.nanoTime();
-				long duration = endTime - startTime;
-				long seconds = (long) (duration/ 1000000000.0);
-				if(textbox.getText().trim().equals(compareEasy)) {
-					JOptionPane.showMessageDialog( f, "Correct! That took you" + seconds + " seconds");
-				}
-				textbox.setText("");
-
-			}
-		}
-
-		if(Main.whichLevel == 2) {
-			if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-				long endTime = System.nanoTime();
-				long duration = endTime - startTime;
-				long seconds = (long) (duration/ 1000000000.0);
-				if(textbox.getText().trim().equals(compareMedium)) {
-					JOptionPane.showMessageDialog( f, "Correct! That took you" + seconds + " seconds");
-				}
-				textbox.setText("");
-			}
-		}
-
-		if(Main.whichLevel == 3) {
-			if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-				long endTime = System.nanoTime();
-				long duration = endTime - startTime;
-				long seconds = (long) (duration/ 1000000000.0);
-				if(textbox.getText().trim().equals(compareHard)) {
-					JOptionPane.showMessageDialog( f, "Correct! That took you" + seconds + " seconds");
-				}
-				textbox.setText("");
-			}
-		}
-
-
-		//find which key is being pressed and change the colour to blue
-		for (int i = 0; i < 57; i++) {
-			if (codes[i] == e.getKeyCode()) {
-				buttons[i].setBackground(Color.decode("#99b3ff"));
-			}
-		}
 	}
-
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		for (int i = 0; i < 57; i++) {
-			if (codes[i] == e.getKeyCode()) {
-				buttons[i].setBackground(null);
-			}
-		}
+		// TODO Auto-generated method stub
+
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+
+	}
+
+
 }
